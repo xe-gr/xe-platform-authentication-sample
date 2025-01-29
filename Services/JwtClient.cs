65c6fc28e7d6aaa0d22124d4bdc1c6c453e12d2f
Ioannis Karadimas <ikaradimas@hotmail.com>
2025-01-29 11:37:46 +0200

    Fixed disposables

--- Services/JwtClient.cs
+++ Services/JwtClient.cs
@@ -11,25 +11,25 @@ namespace XePlatformAuthentication.Services
         public async Task<(HttpStatusCode statusCode, Token token, string body)> GetTokenAsync(
             string clientId, string clientSecret, string userName, string password)
         {
-            using var client = new RestClient(settings.XeAuthUrl);
-            
-            var request = new RestRequest("/auth/realms/xe/protocol/openid-connect/token", Method.Post);
+            using (var client = new RestClient(settings.XeAuthUrl))
+            {
+                var request = new RestRequest("/auth/realms/xe/protocol/openid-connect/token", Method.Post);
 
-            request.AddParameter("client_id", clientId);
-            request.AddParameter("client_secret", clientSecret);
-            request.AddParameter("username", userName);
-            request.AddParameter("password", password);
-            request.AddParameter("grant_type", "password");
-            request.AddParameter("scope", "openid profile email");
+                request.AddParameter("client_id", clientId);
+                request.AddParameter("client_secret", clientSecret);
+                request.AddParameter("username", userName);
+                request.AddParameter("password", password);
+                request.AddParameter("grant_type", "password");
+                request.AddParameter("scope", "openid profile email");
 
-            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
 
-            var response = await client.ExecuteAsync<Token>(request);
+                var response = await client.ExecuteAsync<Token>(request);
 
-            return response.IsSuccessStatusCode 
-                ? (HttpStatusCode.OK, response.Data, string.Empty) 
-                : (response.StatusCode, null, response.Content);
-
+                return response.IsSuccessStatusCode
+                    ? (HttpStatusCode.OK, response.Data, string.Empty)
+                    : (response.StatusCode, null, response.Content);
+            }
         }
     }
 }
